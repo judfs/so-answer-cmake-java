c922f8850aa7637382c6c28325897d2a6888376f
judfs <jstevenson2@udayton.edu>
2025-07-08 01:02:59 -0400

    Example project that makes a sources.jar

---
+++ example-java/com/example/Hello.java
@@ -0,0 +1,11 @@
+package com.example;
+
+class Hello {
+    static void hello() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args){
+        hello();
+    }
+}
